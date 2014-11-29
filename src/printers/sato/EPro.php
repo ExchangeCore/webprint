367fb03ca602b34835fce8e24564cf53e6a09915
joemeyer@exchangecore.com <joemeyer@exchangecore.com>
2014-11-28 19:45:51 -0500

    Added a code 39 php barcode image class

--- src/printers/sato/EPro.php
+++ src/printers/sato/EPro.php
@@ -13,8 +13,6 @@ class EPro extends NetworkPrinter implements NetworkPrinterInterface
     const CAN = "\x18";
     const OFFLINE = "\x40";
 
-    const FONT_SIZE_XS = "XS";
-
     protected $printWidth = 4.1;
     protected $paperWidth = 4;
     protected $dpi = 203;
@@ -33,6 +31,18 @@ class EPro extends NetworkPrinter implements NetworkPrinterInterface
         return true;
     }
 
+    public function setBaseReference($offsetHorizontal = 0, $offsetVertical = 0, $unitOfMeasure = self::UNIT_INCHES)
+    {
+        $printWidth = $this->convertUnitOfMeasure($this->printWidth, self::UNIT_INCHES, self::DOTS_PER_INCH);
+        $paperWidth = $this->convertUnitOfMeasure($this->paperWidth, self::UNIT_INCHES, self::DOTS_PER_INCH);
+        $offsetHorizontal = $this->convertUnitOfMeasure($offsetHorizontal, $unitOfMeasure, self::DOTS_PER_INCH);
+        $offsetHorizontal = str_pad($printWidth - $paperWidth + $offsetHorizontal, 4, '0', STR_PAD_LEFT);
+        $offsetVertical = $this->convertUnitOfMeasure($offsetVertical, $unitOfMeasure, self::DOTS_PER_INCH);
+        $offsetVertical = str_pad(1+$offsetVertical, 4, '0', STR_PAD_LEFT);
+        $this->pushCommand(static::ESC . 'A3H' . $offsetHorizontal . 'V' . $offsetVertical);
+        return $this;
+    }
+
     public function outputText($string)
     {
         $currentPositionHorizontal = $this->convertUnitOfMeasure($this->currentPositionHorizontal, self::UNIT_INCHES, self::DOTS_PER_INCH);
@@ -48,18 +58,6 @@ class EPro extends NetworkPrinter implements NetworkPrinterInterface
             //use a default font
             $this->pushCommand(static::ESC . 'XM' . $string);
         }
-        return $this;
-    }
-
-    public function setBaseReference($offsetHorizontal = 0, $offsetVertical = 0, $unitOfMeasure = self::UNIT_INCHES)
-    {
-        $printWidth = $this->convertUnitOfMeasure($this->printWidth, self::UNIT_INCHES, self::DOTS_PER_INCH);
-        $paperWidth = $this->convertUnitOfMeasure($this->paperWidth, self::UNIT_INCHES, self::DOTS_PER_INCH);
-        $offsetHorizontal = $this->convertUnitOfMeasure($offsetHorizontal, $unitOfMeasure, self::DOTS_PER_INCH);
-        $offsetHorizontal = str_pad($printWidth - $paperWidth + $offsetHorizontal, 4, '0', STR_PAD_LEFT);
-        $offsetVertical = $this->convertUnitOfMeasure($offsetVertical, $unitOfMeasure, self::DOTS_PER_INCH);
-        $offsetVertical = str_pad(1+$offsetVertical, 4, '0', STR_PAD_LEFT);
-        $this->pushCommand(static::ESC . 'A3H' . $offsetHorizontal . 'V' . $offsetVertical);
         return $this;
     }
 
